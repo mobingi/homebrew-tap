bc204af7b62bb2db4388955e1be48e2b1c6d3556
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-30 02:34:52 +0900

    Brew formula update for ouchantools version v1.0.24

--- Formula/ouchanctl.rb
+++ Formula/ouchanctl.rb
@@ -2,16 +2,16 @@
 class Ouchanctl < Formula
   desc "Build tool for ouchan monorepo."
   homepage "https://github.com/mobingi/ouchantools"
-  version "1.0.23"
+  version "1.0.24"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mobingi/ouchantools/releases/download/v1.0.23/ouchantools_1.0.23_darwin_amd64.tar.gz"
-    sha256 "fe7d1b3c85df5045135a90e212d985de7c0252d9f51228afd67c7e1c130e97bf"
+    url "https://github.com/mobingi/ouchantools/releases/download/v1.0.24/ouchantools_1.0.24_darwin_amd64.tar.gz"
+    sha256 "1d62871a26b1b1324edf3fb71c893888b3947e498a1f3073c71201f40511d8eb"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mobingi/ouchantools/releases/download/v1.0.23/ouchantools_1.0.23_linux_amd64.tar.gz"
-      sha256 "b5e6c93c2326142c1b9213449e8246ce90ad250996a706e230fb88ea0ff16432"
+      url "https://github.com/mobingi/ouchantools/releases/download/v1.0.24/ouchantools_1.0.24_linux_amd64.tar.gz"
+      sha256 "2c552465975ef98ca0ab674cea39ad7a2693c58125790f78864cc2f1b5b26d80"
     end
   end
 
