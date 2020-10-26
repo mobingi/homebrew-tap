ddbc653f99b9cf975f8b569727aa0f2c145d712d
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-26 17:20:45 +0900

    Brew formula update for ouchantools version v1.0.39

--- Formula/ccf.rb
+++ Formula/ccf.rb
@@ -2,16 +2,16 @@
 class Ccf < Formula
   desc "Companion tool for ouchan/linkbatchd."
   homepage "https://github.com/mobingi/ouchantools"
-  version "1.0.38"
+  version "1.0.39"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/mobingi/ouchantools/releases/download/v1.0.38/ouchantools_1.0.38_darwin_amd64.tar.gz"
-    sha256 "5d5697759c315b012421f923ca87dfe300837443d5ee2e935eb70f1eebc5adcb"
+    url "https://github.com/mobingi/ouchantools/releases/download/v1.0.39/ouchantools_1.0.39_darwin_amd64.tar.gz"
+    sha256 "c1e47e0f56990733c005078d48722a56570be3e9ee13679b15d6374e1f8521d6"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/mobingi/ouchantools/releases/download/v1.0.38/ouchantools_1.0.38_linux_amd64.tar.gz"
-      sha256 "910e4df754909c6c0a558ebe31c8c6df12e0e21f12d3b30ca59193ba0b9c56f2"
+      url "https://github.com/mobingi/ouchantools/releases/download/v1.0.39/ouchantools_1.0.39_linux_amd64.tar.gz"
+      sha256 "a01f5f50dba94702bf18a64ed6ba4a9ac44a9d72e88758ae2b5d471e7d591ae0"
     end
   end
 
